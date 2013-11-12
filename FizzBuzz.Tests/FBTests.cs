b62726dea73a8a5987059e6f2be00d5555400720
IretonWeb <info@iretonweb.co.uk>
2013-11-12 12:40:08 +0000

    Fizz Buzz Step 1

---
+++ FizzBuzz.Tests/FBTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    class FBTests
+    {
+
+
+        [Test]
+        public void MulitpleNumber3Test()
+        {
+
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizz", FizzBuzz.WorkOut(3));
+        }
+
+        [Test]
+        public void Number5Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("buzz", FizzBuzz.WorkOut(5));
+        }
+
+        [Test]
+        public void Number15Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizzbuzz", FizzBuzz.WorkOut(15));
+        }
+
+        [Test]
+        public void DifferentNumberTest()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("1", FizzBuzz.WorkOut(1));
+            
+        }
+       
+       
+
+    }
+
+    
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz
+    {
+
+        private static int startVal = 1;
+        private static int endVal = 20;
+
+        public static void Main()
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = startVal; i <= endVal; i++)
+            {
+                output.Append(WorkOut(i) + ' ');
+            }
+
+           Console.WriteLine("{0}",output.ToString());
+        }
+
+        public static string WorkOut(int number)
+        {
+            bool number3 = ((number % 3) == 0);
+            bool number5 = ((number % 5) == 0);
+      
+            if (number3 && number5)
+            {
+                return "fizzbuzz";
+            }
+            else if(number3)
+            {
+                return "fizz";
+            }
+            else if (number5)
+            {
+                return "buzz";
+            }
+           
+            else
+            {
+                return number.ToString();
+            }
+            
+        }
+       
+    }
+}
